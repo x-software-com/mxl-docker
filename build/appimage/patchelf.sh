7401910356cd3c064ad28d8a2028a7f8b1e43fe9
marcbull <31337222+marcbull@users.noreply.github.com>
2024-07-12 12:43:52 +0200

    chore: use AlmaLinux8 as the base image for build:3 and use appimage-setup submodule

--- build/appimage/patchelf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-if [ -z ${NO_STRIP} ] || [ "${NO_STRIP}" == "0" ]; then
-	FILENAME="$(echo $@ | awk '{print $NF}')"
-	strip --strip-debug --strip-unneeded ${FILENAME}
-fi
-
-/opt/linuxdeploy/usr/bin/patchelf.binary $@
